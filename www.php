5d53453138e31793e8a60ec8d1e98457d77f31fe
Shivam-tomar9 <130753871+Shivam-tomar9@users.noreply.github.com>
2024-03-11 17:19:08 +0530

    htacess and index

--- www.php
+++ www.php
@@ -1,9 +1,29 @@
-$actual_link = $_SERVER['HTTP_HOST'].''.$_SERVER['REQUEST_URI'];
+//do the changes in your htacess file
 
-if (strpos($actual_link, 'www.') === true) {
-    $url = str_replace('www.', '', $actual_link);
+
+RewriteEngine On
+
+# Force HTTPS and non-www
+RewriteCond %{HTTPS} !=on [OR]
+RewriteCond %{HTTP_HOST} ^www\.greatinflux\.com [NC]
+RewriteRule ^ https://greatinflux.com%{REQUEST_URI} [L,R=301]
+
+# Remove trailing slash
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule ^(.+)/$ /$1 [L,R=301]
+
+# Rewrite everything to index.php
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteRule ^ index.php [L]
+
+//for removing public 
+$requestUri = $_SERVER['REQUEST_URI'];
+if (strpos($requestUri, '/public/') !== false) {
+    // Remove "public" from the URI
+    $newUri = str_replace('/public', '', $requestUri);
 
     // Redirect to the new URL
-    header('Location: https://'.$url);
+    header('Location: https://greatinflux.com' . $newUri, true, 301);
     exit();
 }
